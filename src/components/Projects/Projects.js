b42a71ef177056aa1cecc18037a501f32e8ed38c
Romain BERNAZ <bernazromain@gmail.com>
2020-11-08 15:19:04 +0100

    weather

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Projects.css';
 import  random from '../../images/random.png'
 import  todo from '../../images/todo.png'
+import weather from '../../images/weather.png'
 
 
 const Projects = () => {
@@ -13,27 +14,31 @@ const Projects = () => {
             <div class="photo">
                 <span class="photo01">PROJECTS</span>
                 <span class="photo02">PROJECTS</span>
-                <span class="photo03">PROJECTS</span>
-                <span class="photo04">PROJECTS</span>
-                <span class="photo05">PROJECTS</span>
-                <span class="photo04">PROJECTS</span>
-                <span class="photo05">PROJECTS</span>
-                <span class="photo04">PROJECTS</span>
+                <span class="photo01">PROJECTS</span>
+                <span class="photo02">PROJECTS</span>
+                <span class="photo01">PROJECTS</span>
+                <span class="photo02">PROJECTS</span>
+                <span class="photo01">PROJECTS</span>
+                <span class="photo02">PROJECTS</span>
 
             </div>
 
-        <div className="random">
-           <a href="https://romainbernaz.github.io/randomNumberReact/"><img src={random} alt="random"/></a>
-           <div className="todo">
-           <a href="https://romainbernaz.github.io/todoList-react/"><img src={todo} alt=""/></a> 
+<div className="imageList">
+
+           <div className="random">
+               <a href="https://romainbernaz.github.io/randomNumberReact/"><img src={random} alt="random"/></a>
            </div>
+           <div className="todo">
+               <a href="https://romainbernaz.github.io/todoList-react/"><img src={todo} alt=""/></a> 
+           </div>
+           <div className="weather">
+                <a href="https://romainbernaz.github.io/weatherApp/"><img src={weather} alt=""/></a>
+        </div>
+        </div>
         </div>
 
 
 
-
-
-        </div>
     )
 }
 
