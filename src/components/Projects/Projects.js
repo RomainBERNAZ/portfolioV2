0d2493bfa0a261dfe2300ace9833b67720622ce0
Romain BERNAZ <bernazromain@gmail.com>
2020-12-02 12:10:33 +0100

    before sending

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Projects.css';
 import random from '../../images/random.png'
 import todo from '../../images/todo.png'
+import recipe from '../../images/recipe.png'
 import weather from '../../images/weather.png'
 import twitter from '../../images/twitter.png'
 import fairmade from '../../images/fairmade.png'
@@ -33,14 +34,14 @@ const Projects = () => {
                     <div className="fairmadeSquare"></div>
                 </div>
                 <div className="fairmadeText">
-                <div className="fairmadeTitle">FAIRMADE</div>
+                <div className="fairmadeTitle">FAIRMADE.FR</div>
                     <div className="fairmadeExp">
                         Site Web e-commerce WordPress <br/>
                         Template Shop-Isle <br/>
                         Accompagnement client complet <br/>
-                        CSS personnalisé 
-                        
+                        CSS personnalisé <br/>
                     </div>
+                <a target="_blank" href="https://www.fairmade.fr"> <i  class="fas fa-arrow-right"></i></a>
                 </div>
         </div>
 
@@ -54,6 +55,7 @@ const Projects = () => {
                         NODE JS
                         
                     </div>
+                    <a target="_blank" href="https://twitter.com/ParkNantesBot"> <i  class="fas fa-arrow-right"></i></a>
                 </div>
                 <div className="twitterImage">
                     <img src={twitter} alt="" className="twitter"/>
@@ -73,12 +75,32 @@ const Projects = () => {
                         Donne la météo de la ville entrée en paramètre <br/>
                         Affiche la ville, une icone et la température<br/>
                         Fonctionne avec l'API OpenWeatherMap  <br/>
-                        HTML / CSS / JAVASCRIPT 
+                        HTML / CSS / REACT 
                         
                     </div>
+                    <a target="_blank" href="https://romainbernaz.github.io/weatherApp/"> <i  class="fas fa-arrow-right"></i></a>
                 </div>
         </div>
 
+        <div className="recipeBlock">
+                <div className="recipeText">
+                    <div className="recipeTitle">RECIPE APP</div>
+                    <div className="recipeExp">
+                        V1 <br/>
+                        Création de compte et login / Déconnexion<br/>
+                        Création / Suppression de recette<br/>
+                        Gestion des routes utilisateurs <br/>
+                        Code sur GitHub / En ligne prochainement <br/>
+                        REACT / SASS / STRAPI / AXIOS
+                        
+                    </div>
+                    <a href="https://www.fairmade.fr"> <i  class="fas fa-arrow-right"></i></a>
+                </div>
+                <div className="recipeImage">
+                    <img src={recipe} alt="" className="recipe"/>
+                    <div className="recipeSquare"></div>
+                </div>
+        </div>
 
 
         </div>
